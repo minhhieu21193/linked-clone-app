62868bb68952e49b9ae1adc6d06a0ed9b74494d0
Bem <minhhieu21193@gmail.com>
2022-11-11 22:46:16 +0700

    first commit

--- pages/home.js
+++ pages/home.js
@@ -7,7 +7,7 @@ function home() {
       <header>
         <div className="relative w-36 h-10">
           <Image
-            src="https://rb.gy/vtbzlp"
+            src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Logo.svg.original.svg"
             layout="fill"
             objectFit="contain"
             alt=""
